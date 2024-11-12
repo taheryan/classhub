03a181f81dc76d51e899dfdfe050c5f7468e044c
Erfan Taheryan <erfantaheryan@gmail.com>
2024-11-12 09:22:21 +0330

    course model rename

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use CodeIgniter\Model;
 
-class CourseModel extends Model
+class Course extends Model
 {
     // Define the table name for this model
     protected $table      = 'courses';
